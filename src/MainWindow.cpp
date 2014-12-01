1ec9dd686f878c8387468ca5738f9c5aacd8dc20
Sebastian Morr <sebastian@morr.cc>
2014-12-01 20:21:10 +0100

    Assign a few keybindings to the top menu's buttons

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -48,6 +48,10 @@ MainWindow::MainWindow() :
     toolbar.append(toolbutton_sep2);
     toolbar.append(toolbutton_zoomin);
     toolbar.append(toolbutton_zoomout);
+
+    Glib::RefPtr<Gtk::AccelGroup> accel_group = Gtk::AccelGroup::create();
+    add_accel_group(accel_group);
+
     toolbutton_add.signal_clicked().connect(sigc::mem_fun(*this,&MainWindow::on_add_clicked));
     toolbutton_edit.signal_clicked().connect(sigc::mem_fun(*this,&MainWindow::on_edit_clicked));
     toolbutton_left.signal_clicked().connect(sigc::mem_fun(*this,&MainWindow::on_left_clicked));
@@ -55,6 +59,13 @@ MainWindow::MainWindow() :
     toolbutton_remove.signal_clicked().connect(sigc::mem_fun(*this,&MainWindow::on_remove_clicked));
     toolbutton_zoomin.signal_clicked().connect(sigc::mem_fun(*this,&MainWindow::on_zoomin_clicked));
     toolbutton_zoomout.signal_clicked().connect(sigc::mem_fun(*this,&MainWindow::on_zoomout_clicked));
+
+    toolbutton_add.add_accelerator("clicked", accel_group, GDK_KEY_plus, (Gdk::ModifierType) 0, (Gtk::AccelFlags) 0);
+    toolbutton_left.add_accelerator("clicked", accel_group, GDK_KEY_Left, Gdk::CONTROL_MASK, (Gtk::AccelFlags) 0);
+    toolbutton_right.add_accelerator("clicked", accel_group, GDK_KEY_Right, Gdk::CONTROL_MASK, (Gtk::AccelFlags) 0);
+    toolbutton_remove.add_accelerator("clicked", accel_group, GDK_KEY_Delete, (Gdk::ModifierType) 0, (Gtk::AccelFlags) 0);
+    toolbutton_zoomin.add_accelerator("clicked", accel_group, GDK_KEY_plus, Gdk::CONTROL_MASK, (Gtk::AccelFlags) 0);
+    toolbutton_zoomout.add_accelerator("clicked", accel_group, GDK_KEY_minus, Gdk::CONTROL_MASK, (Gtk::AccelFlags) 0);
 
     //black toolbars in ubuntu
     Glib::RefPtr<Gtk::StyleContext> sc = toolbar.get_style_context();
